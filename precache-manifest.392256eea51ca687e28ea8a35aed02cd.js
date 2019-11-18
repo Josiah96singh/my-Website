c88128e524863bc7a0911549e37b9c65812be894
Josiah <Josiahsingh1996@gmail.com>
2019-11-18 15:54:08 +0200

    Updates

---
+++ precache-manifest.392256eea51ca687e28ea8a35aed02cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45be20df8eb8ec606a686e0e9b50faa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6080a0f5c601a9761d22",
+    "url": "/static/css/main.c53e4fbc.chunk.css"
+  },
+  {
+    "revision": "82aef7738f5ca62175c0",
+    "url": "/static/js/2.4cf8e87e.chunk.js"
+  },
+  {
+    "revision": "6080a0f5c601a9761d22",
+    "url": "/static/js/main.d7abaa02.chunk.js"
+  },
+  {
+    "revision": "b928d249e2ed4f7f6d62",
+    "url": "/static/js/runtime-main.4ddb6e0d.js"
+  },
+  {
+    "revision": "a4d680bf6db68f28e4bbd906568f6d9a",
+    "url": "/static/media/Resume.a4d680bf.pdf"
+  },
+  {
+    "revision": "b0d42ff88cc0c6dbf547551b6eb8124d",
+    "url": "/static/media/news1(gimp).b0d42ff8.png"
+  },
+  {
+    "revision": "4b89da8375f5ea074c74d60252aa9bab",
+    "url": "/static/media/unsplash.4b89da83.webm"
+  }
+]);
